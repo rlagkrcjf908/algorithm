0e3143d139a7f371e68ff333c830effcee74e472
rlagkrcjf908 <83570146+rlagkrcjf908@users.noreply.github.com>
2023-09-14 23:15:39 +0900

    [Bronze V] Title: 나머지, Time: 104 ms, Memory: 12792 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    // 나머지
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = Integer.parseInt(sc.next());
+        int b = Integer.parseInt(sc.next());
+        int c = Integer.parseInt(sc.next());
+
+        System.out.println((a+b)%c);
+        System.out.println(((a%c)+(b%c))%c);
+        System.out.println((a*b)%c);
+        System.out.println(((a%c)*(b%c))%c);
+    }
+}
